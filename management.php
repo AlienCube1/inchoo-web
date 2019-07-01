a889c75801f3eb8db356f52c0ea34f1a48a69526
AlienCube1 <51066647+AlienCube1@users.noreply.github.com>
2019-07-01 04:34:56 +0200

    Changed management
    Added basic file upload for management, need to sort them to tables now

--- management.php
+++ management.php
@@ -1,9 +1,12 @@
-<?php
+<!DOCTYPE html>
+<html>
+<body>
 
+<form action="upload.php" method="post" enctype="multipart/form-data">
+    
+    <input type="file" name="" id="fileToUpload">
+    <input type="submit" class="plus-button" value="Upload Image" name="submit">
+</form>
 
-
-
-
-
-
-?>
+</body>
+</html>
