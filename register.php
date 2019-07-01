5f8b9ea6755697b7dccfb690a70ecb85dee9b421
AlienCube1 <51066647+AlienCube1@users.noreply.github.com>
2019-07-01 02:50:00 +0200

    Added all the neccessary files
    Main is the home page where I am working the most currently, management is the place for users to upload pictures, stilistika is a file for css, other files i'll comment in next commits.

---
+++ register.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+<form action="login.php" method = 'post' id="anID">
+	<input type="text" name="username">
+	<input type ="text" name="mail">
+	<input type ="password" name ="password">
+	<input type ="password" name ="repeatePassword">
+	<input type ="submit" value="Login">
+	<input type="checkbox" name="remember" id="remember"/>
+	<label for="remember-me">Remember me</label>
+</form>
+</body>
+</html>
